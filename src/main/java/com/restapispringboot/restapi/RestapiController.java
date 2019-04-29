41f7fe35621d5ac535e22e7414b0c6188b474e87
yhayashi30 <yoshiyuki.hayashi@unisys.co.jp>
2019-04-29 22:06:12 +0900

    controllre,applicationを正しく修正

--- src/main/java/com/restapispringboot/restapi/RestapiController.java
+++ src/main/java/com/restapispringboot/restapi/RestapiController.java
@@ -5,14 +5,10 @@ import java.util.List;
 import com.restapispringboot.restapi.util.OAuth2Salesforce;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
 import org.springframework.security.oauth2.provider.OAuth2Authentication;
 import org.springframework.web.bind.annotation.*;
 
-@SpringBootApplication
 @RestController
-@EnableOAuth2Sso
 public class RestapiController{
 
     @Autowired
